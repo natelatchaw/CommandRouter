66f49b53db6487cb4c77b0d018064e392a0fbd22
Nathan Latchaw <natelatchaw@gmail.com>
2021-05-10 16:33:05 -0500

    Initial commit

---
+++ router/__init__.py
@@ -0,0 +1,8 @@
+"""
+Command Router
+
+A module-based command handler.
+"""
+
+__version__ = '0.0.2'
+__author__ = 'Nathan Latchaw'
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='router',
+    version='0.0.2',
+    description='A module-extensible message router',
+    author='Nathan Latchaw',
+    author_email='natelatchaw@gmail.com',
+    license='Creative Commons',
+    packages=['router', 'router.configuration', 'router.static'],
+    url='not available',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: Alpha',
+        'Intended Audience :: Hobbyist',
+        'Operating System :: Agnostic',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
