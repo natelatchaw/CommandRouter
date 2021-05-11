43fe7d6eb5ab84c723b6b56b3d3bad48c32b209d
Nathan Latchaw <natelatchaw@gmail.com>
2021-05-11 16:11:08 -0500

    removed discord dependency

--- router/logger.py
+++ router/logger.py
@@ -1,22 +1,11 @@
-import discord
 from router.settings import Settings
 
 class Logger():
     
-    def __init__(self, settings: Settings, guild: discord.Guild):
+    def __init__(self, settings: Settings):
         self.settings = settings
-        self.guild = guild
 
     async def print(self, *args):
         message = '\n'.join(args)
-        try:
-            # get the logging channel id from the config
-            channel_id: int = self.settings.logging_channel
-        except (TypeError, ValueError):
-            return
-        # get the channel object from the channel id
-        logging_channel = self.guild.get_channel(channel_id)
-        if not isinstance(logging_channel, discord.TextChannel):
-            return
-        await logging_channel.send(f'{message}')
+        print(message)
 
