99134ca4406675d72d86352e9e9aa8596d9d58fe
Nathan Latchaw <natelatchaw@gmail.com>
2021-05-11 16:34:43 -0500

    moved .gitignore, started test folder

---
+++ test/test_handler.py
@@ -0,0 +1,11 @@
+import pytest
+from router.handler import Handler
+
+class TestHandler:
+    def test_handler(self):
+        """
+        Test to see if loading an invalid path throws a TypeError
+        """
+        handler: Handler =  Handler()
+        with pytest.raises(TypeError):
+            handler.load('')
