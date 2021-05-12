c311f09b4ccef609470ae01ca24b0b7508cffd65
Nathan Latchaw <natelatchaw@gmail.com>
2021-05-12 12:49:45 -0500

    added test

--- router/test/test_handler.py
+++ router/test/test_handler.py
@@ -1,11 +1,25 @@
+import pathlib
 import pytest
 from router.handler import Handler
 
 class TestHandler:
-    def test_handler(self):
+    def test_handler_load_invalid_directory(self):
         """
         Test to see if loading an invalid path throws a TypeError
         """
         handler: Handler =  Handler()
         with pytest.raises(TypeError):
             handler.load('')
+
+    def test_handler_load_empty_directory(self, tmp_path: pathlib.Path):
+        """
+        Check behavior on loading empty directory
+        """
+        # generate path for test module folder
+        test_modules: pathlib.Path = tmp_path.joinpath('test_modules')
+        # make the directory from the path
+        test_modules.mkdir(parents=True)
+        # initialize Handler instance
+        handler: Handler = Handler()
+        # load the test module folder
+        handler.load(test_modules)
